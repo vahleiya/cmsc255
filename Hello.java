414b9efbcc46ec3ecc0c60f61558c47ef87c7e0e
vahleiya <54388202+vahleiya@users.noreply.github.com>
2019-08-22 11:02:36 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,26 @@
+/************************************************************************
+* Hello
+*************************************************************************
+* To print text on screen
+*________________________________________________________________________
+* Gabriella Desmond
+* August 21 2019
+* Course 255 Section 1
+************************************************************************/
+
+public class Hello {
+  public static void printHeading() {
+    System.out.println("Gabriella Desmond");
+    System.out.println("Hello");
+    System.out.println("To print text on screen");
+    System.out.println("August 21 2019");
+    System.out.println("Course 255 Section 1");
+    System.out.println();
+   }
+
+	public static void main(String [] args ) {
+	  printHeading();
+	  System.out.println("Hello");
+
+   }
+}
